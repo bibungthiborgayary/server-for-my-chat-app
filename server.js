ad6ae6458902f1021144d23539cd46f5ae21ed2b
bibungthi053 <bibungthi1230@gmail.com>
2024-10-14 20:31:19 +0530

    Update CORS origin to allow requests from 'https://bibung.netlify.app'

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://localhost:5173'],
+        origin: ['https://bibung.netlify.app'],
         methods: ['GET', 'POST'],
         credentials: true,
     },
 });
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: ['https://bibung.netlify.app'],
     methods: ['GET', 'POST'],
     credentials: true,
 }));
